c54362d9feedc577b8dc944227c1406022f5f671
Kacper Nikiel <ziomus12.07@gmail.com>
2025-03-13 11:58:24 +0100

    Dodaj pliki projektów.

---
+++ Controllers/ToDoItemController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using restapitodo.Data;
+using restapitodo.Models;
+
+namespace restapitodo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoItemController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ToDoItemController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task <ActionResult<List<ToDoItem>>> GetAllToDoItems()
+        {
+            var toDoItems = await _context.ToDoItems.ToListAsync();
+            return Ok(toDoItems); 
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<List<ToDoItem>>> GetToDo(int id)
+        {
+            var toDoItems = await _context.ToDoItems.FindAsync(id);
+            if (toDoItems is null)
+                return NotFound("Nie znaleziono zadania");
+            return Ok(toDoItems);
+        }
+        [HttpPost]
+        [Route("{id}")]
+        public async Task<ActionResult<List<ToDoItem>>> GetToDo(int id)
+        {
+            var toDoItems = await _context.ToDoItems.FindAsync(id);
+            if (toDoItems is null)
+                return NotFound("Nie znaleziono zadania");
+            return Ok(toDoItems);
+        }
+    }
+}
---
+++ Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using restapitodo.Models;
+
+namespace restapitodo.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+    }
+}
